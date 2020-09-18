aef9d10c9282ecf522aa5a1ce46cdf69c73b8021
wang_j11 <jacking.wang.wjq@gmail.com>
2020-09-18 13:19:38 +0800

    Add two esay sulotions

---
+++ easy/cpp/subdomainVisitCount/test.cpp
@@ -0,0 +1,15 @@
+/*************************************************************************
+    > File Name: test.cpp
+    > Author: jaycee
+    > Mail: jaycee_wjq@163.com
+    > Date: 26/10/19 17:06:25 +0800
+ ************************************************************************/
+
+#include <stdio.h>
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    []{};
+    return 0;
+}
